332191f772a8612b1792abf356004ceca7570f6d
Mirra Chvaliuk <mirra9002@gmail.com>
2025-05-17 11:39:31 +0300

    Adding react-routing version 0.0.4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+import Mainpage from './Pages/MainPage.jsx'
+import Courseinfo from './Pages/CourseInfo.jsx'
+import Notfound from './Pages/NotFound.jsx';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Mainpage />,
+  errorElement: <Notfound/>
+},
+{
+  path: '/courseinfo',
+  element: <Courseinfo />,
+  errorElement: <Notfound/>
+},])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
